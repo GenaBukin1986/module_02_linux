272b6962107210b74cda10cd71ab4f963dfd121f
Nick_Bear <bear_76@mail.ru>
2025-03-03 20:17:09 +0300

    Updated functions for page output: - Added "Hello World" page display - Implemented current time display - Added feature to show exact time after one hour - Included page visit counter functionality - Integrated random word selection from the book "War and Peace"

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,43 +1,66 @@
 import datetime
+import random
+from get_word import get_word
+
 from flask import Flask
 
 app = Flask(__name__)
 
+list_cars = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+list_cats = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
+book = get_word()
+
 
 @app.route('/hello_world')
-def test_function():
-    pass
+def get_hello_world():
+    """Возвращает строку 'Привет, мир!'"""
+    return '<h2>Привет, мир!</h2>'
 
 
 @app.route('/cars')
-def test_function():
-    pass
+def get_list_cars():
+    """Возвращает случайно выбранную машину из списка машин"""
+    global list_cars
+    return f'<h2>Список машин:</h2>\n<h3>{", ".join(list_cars)}</h3>'
 
 
 @app.route('/cats')
-def test_function():
-    pass
+def get_random_cats():
+    """Возвращает случайно выбранную породу кошек из списка пород"""
+    global list_cats
+    return f'<h2>Эта страница про кошек.</h2>\n' \
+           f'<h3>Сегодня мы познакомимся с породой: {random.choice(list_cats)}</h3>'
 
 
 @app.route('/get_time/now')
-def test_function():
-    pass
+def get_time_now():
+    """Возвращает точное время"""
+    return f'<h2>Точное время: {datetime.datetime.now()}</h2>'
 
 
 @app.route('/get_time/future')
-def test_function():
-    pass
+def get_time_future():
+    """Возвращает точное время через час"""
+    current_time_after_hour = datetime.timedelta(hours=1)
+    return f'Точное время через час будет {current_time_after_hour}'
 
 
 @app.route('/get_random_word')
-def test_function():
-    pass
+def get_random_word():
+    """Возвращает случайное слово из книги 'Война и мир' Льва Толстого"""
+    global book
+    return f'<h2>Эта страница отображает случайное слово из книги "Война и мир" Льва Толстого</h2>\n' \
+           f'<h3>Случайное слово из книги: <i>{random.choice(book)}</i></h3>'
 
 
 @app.route('/counter')
-def test_function():
-    pass
+def get_counter_page():
+    """Возвращает сколько раз открывалась страница"""
+    get_counter_page.visits += 1
+    return f'Эта страница открывалась {get_counter_page.visits} раз'
 
+
+get_counter_page.visits = 0
 
 if __name__ == '__main__':
     app.run(debug=True)
